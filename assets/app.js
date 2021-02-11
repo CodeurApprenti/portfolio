6cf378e26e6b3010aaa22f23f7aeec1d61e9845b
Ledoux Kevin <ledoux.kevin@yahoo.com>
2021-02-11 14:51:21 +0100

    Make of use webpack encore

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+
+import "./scss/app.scss";
+
+// start the Stimulus application
+
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap';
